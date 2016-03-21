70da3d996e4adfff37af558823ddec93f71d0eb3
smurfik <smurfik@mlpinit.com>
2016-03-20 20:04:33 -0700

    add a text field to change the city

--- app/assets/javascripts/search_studio.js
+++ app/assets/javascripts/search_studio.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
-  // $('.city')
-  // var geocoder = new google.maps.Geocoder();
-  // console.log($('.city').text());
-  // console.log(geocoder.geocode({address: $('.city').text()}));
+  $('button#city_btn').click( function(e) {
+    e.preventDefault();
+    var query = $('input#city').val() || "Seattle";
 
-  // get ajax request
-  // https://maps.googleapis.com/maps/api/geocode/json?address=Seattle&key=GOOGLE_API_KEY
-  $.get("/search_google", {
-    city: $('.city').text()
-    }, function(data) {
-      $('.city').html(data.address)
-      console.log($("h1"));
-      // console.log(data);
+    $.get("/search_google", {
+      city: query
+      }, function(data) {
+        $('h1').html("The list of studios in " + data.address)
+    });
   });
-
 });
