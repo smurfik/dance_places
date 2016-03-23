13b35d2dc44a7dde5ec6d714d9b4d330c0fec712
smurfik <smurfik@mlpinit.com>
2016-03-23 14:26:36 -0700

    only add studios to db that have facebook id

--- app/models/yelp_data.rb
+++ app/models/yelp_data.rb
@@ -2,6 +2,7 @@ require 'yelp'
 class YelpData < ActiveRecord::Base
 
   def self.add_studios(lat, lng)
+    @graph = Facebook.graph
     # coordinates = { latitude: 47.6097, longitude: -122.3331 }
     coordinates = { latitude: lat, longitude: lng }
     info = { term: 'dance',
@@ -10,16 +11,36 @@ class YelpData < ActiveRecord::Base
     }
     result = Yelp.client.search_by_coordinates(coordinates, info)
     result.businesses.each do |studio|
-      st          = Studio.new
-      st.name     = studio.name
-      st.street   = studio.location.address[0]
-      st.city     = studio.location.city
-      st.state    = studio.location.state_code
-      st.zip_code = studio.location.postal_code
-      st.lat      = studio.location.coordinate.latitude
-      st.lng      = studio.location.coordinate.longitude
-      st.save
+      id = get_studio(studio)
+      if id != "Error"
+        st          = Studio.new
+        st.name     = studio.name
+        st.street   = studio.location.address[0]
+        st.city     = studio.location.city
+        st.state    = studio.location.state_code
+        st.zip_code = studio.location.postal_code
+        st.lat      = studio.location.coordinate.latitude
+        st.lng      = studio.location.coordinate.longitude
+        st.facebook_id = id["id"]
+        st.save
+        add_website_image(st, id["id"])
+      end
     end
+  end
+
+  def self.get_studio(studio)
+    @graph.get_object(studio.name.gsub(/\s+|'|:/, ""))
+  rescue URI::InvalidURIError, Koala::Facebook::ClientError => e
+    "Error"
+  end
+
+  def self.add_website_image(studio, facebook_id)
+    url = @graph.get_connections(facebook_id, "?fields=website")
+    studio.update_attribute(:url, url["website"].split[0]) unless !url["website"].present?
+    image = @graph.get_connections(facebook_id, "?fields=photos{album,images}")
+    studio.update_attribute(:image, image["photos"]["data"][0]["images"][0]["source"]) unless !image["photos"]["data"][0]["images"][0]["source"].present?
+    # image = @graph.get_connections(studio.facebook_id, "?fields=picture{url}")
+    # studio.update_attribute(:image, image["picture"]["data"]["url"]) unless !image["picture"]["data"]["url"].present?
   end
 
 end
